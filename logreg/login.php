129d39c11a1da58c95a0b646f3df1c29aedefa03
SanaBw <sanida.fatic@stu.ibu.edu.ba>
2019-04-02 01:43:40 +0200

    hotel db

--- logreg/login.php
+++ logreg/login.php
@@ -10,7 +10,7 @@ include('connection.php');
 $loginEmail=mysqli_real_escape_string($conn, ($_POST['login-email']));
 $loginPassword=mysqli_real_escape_string($conn, ($_POST['login-password']));
 
-$query = "select email, password, name, status from users where email='$loginEmail'";
+$query = "select ID, email, password, name, status from users where email='$loginEmail'";
 $result = mysqli_query($conn, $query);
 $vals="";
 
@@ -22,6 +22,7 @@ if(mysqli_num_rows($result) > 0){
         $vals="status";
     } else if (password_verify($loginPassword,$data['password'])){
         session_start();
+        $_SESSION['ID'] = $data['ID'];
         $_SESSION['name'] = $data['name'];        
         $_SESSION['email'] = $data['email'];
         $_SESSION['loggedin'] = true;        
